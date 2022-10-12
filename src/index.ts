13d0328c745810c72be088426b3331fe346f2a61
tiohs <hamiltonsilva551@gmail.com>
2022-10-12 20:47:10 +0100

    feat: :fire: add swagger

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from 'swagger.json';
 
 import cors from 'cors';
 
@@ -7,7 +8,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerUi));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.get('/', (request, response) => {
   return response.status(200).json({
     message: 'Hello World! Init your project nodejs !',
